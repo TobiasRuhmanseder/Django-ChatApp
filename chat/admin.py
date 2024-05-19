d9ce7719f0697b81c9eb7340c5e389c209e20b65
Tobias Ruhmanseder <tobi.r1707@gmail.com>
2024-05-19 13:16:11 +0200

    update login annotation

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,19 @@
+from .models import Chat, Message
 from django.contrib import admin
 
+
+class MessageAdmin(admin.ModelAdmin):
+    fields = (
+        "chat",
+        "text",
+        "created_at",
+        "author",
+        "receiver",
+    )  # cutom display of data field / default u don' need this - use only admin.site.register(Message)
+    list_display = ("text", "created_at", "author", "receiver")
+    search_fields = ("created_at",)  # sorts the table depending on the input parameters
+
+
 # Register your models here.
+admin.site.register(Message, MessageAdmin)
+admin.site.register(Chat)
